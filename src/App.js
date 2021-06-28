d2d521b46fdc6d5ee6e48a5fd4d7d92a1f4b37db
Elena Ru <elenaru@hotmail.de>
2021-06-28 20:42:54 +0200

    react lists

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 import Counter from './components/Counter';
  
 import Emoji from './components/Emoji';
+
+import { listItems, MoviesList } from "./components/ListDemo";
+
+import DynamicMoviesList from "./components/dynamicListsDemo/DynamicMoviesList.js";
  
 function App() {
   return (
     <div className="App">
       <Counter />
       <Emoji/>
+      <ul>{listItems}</ul>
+      <div>
+          <MoviesList />
+        </div>
+        <hr />
+        <DynamicMoviesList />
     </div>
   );
 }
